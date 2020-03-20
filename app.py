a5e4a464960e2a6fd069b7c5d046b3a9922fe2d9
hassan jalil <hassan.jalil576@gmail.com>
2020-03-20 14:49:42 +0000

    some changes

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import atexit
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, jsonify
-
+from flask_cors import CORS, cross_origin
 from models import db
 from utils.hourly_task import hourly_task
 from utils.parse_requests import get_latest_data_for_count, get_country_data_for_time
 
 app = Flask(__name__)
 app.config.from_object('config.config')
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 db.init_app(app)
 db.app = app
 
@@ -33,11 +34,13 @@ def hello_world():
 
 
 @app.route('/country/<country_name>', methods=['GET'])
+@cross_origin()
 def get_country_data(country_name: str):
     return jsonify(get_latest_data_for_count(country_name))
 
 
 @app.route('/country/<country_name>/<time>', methods=['GET'])
+@cross_origin()
 def get_country_for_time_frame(country_name: str, time: str):
     data = get_country_data_for_time(country_name, time)
     return jsonify(data)
